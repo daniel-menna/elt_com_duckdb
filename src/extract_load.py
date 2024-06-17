6629a0bcbdbe5d1d8512418b84a68a5c9bded45e
Daniel Menna <danielribeiromenna@gmail.com>
2024-06-17 17:24:34 -0300

    feat: setup variaveis de ambiente

--- src/extract_load.py
+++ src/extract_load.py
@@ -7,7 +7,20 @@ import os
 
 # import das minhas variaveis de ambiente
 
+load_dotenv()
+
 commodities = ['CL=F', 'GC=F', 'SI=F']
+
+DB_HOST=os.getenv('DB_HOST_PROD')
+DB_PORT=os.getenv('DP_PORT_PROD')
+DB_NAME=os.getenv('DB_NAME_PROD')
+DB_USER=os.getenv('DB_USER_PROD')
+DB_PASS=os.getenv('DB_PASS_PROD')
+DB_SCHEMA=os.getenv('DB_SCHEMA_PROD')
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL)
 
 def buscar_dados_commodities(ticker, periodo='5d', intervalo='1d'):
     ticker = yf.Ticker(ticker)
@@ -22,11 +35,9 @@ def buscar_todos_commodities(commodities):
         todos_dados.append(dados)
     return pd.concat(todos_dados)
 
-# pegar a cotação dos meus ativos
-
-# concatenar os meus ativos (1...2..3) -> (1)
-
+def salvar_no_postgres(df, schema='public'):
+    df.to_sql('commodities', engine, if_exists='replace', index=True, index_label='Data', schema=schema )
 
 if __name__ == "__main__":
     dados_concatenado = buscar_todos_commodities(commodities)
-    print(dados_concatenado)
+    salvar_no_postgres(dados_concatenado, schema='public')
